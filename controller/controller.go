d761ade69d2635751fed3d67bbeea94e7d767e2e
k-goto <k-goto@masagent.co.jp>
2022-12-23 10:36:53 +0900

    csvの列確定

--- controller/controller.go
+++ controller/controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"encoding/csv"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"golang.org/x/text/encoding/japanese"
@@ -27,27 +28,40 @@ func Regit_Date(ctx *gin.Context) {
 	var line []string
 	//  二次元配列に格納
 	var Csv_Result_2nd_ary [][]string
+	var lenary []string
+	
+	col_cnt := 0
 
 	for {
 		// 行毎のデータ取得してlineに格納
 		line, err = reader.Read()
-
+		
+		
 		if err != nil {
 			break
 		}
+		
+		
+		if col_cnt == 0 {
+			col_cnt = len(line)
+			
+		
+			for i := 1; i < len(line) + 1; i++ {
+				var in_str int = i
+				s := strconv.Itoa(in_str)
+				lenary = append(lenary, s)
+			}
+			
+			Csv_Result_2nd_ary = append(Csv_Result_2nd_ary, lenary)
+		}
+
 
 		// 二次元配列に追加
 		Csv_Result_2nd_ary = append(Csv_Result_2nd_ary, line)
 	}
 
-	// fmt.Printf("%v", array)
 	// reader.LazyQuotes = true
-
-	// ファイル名
-	// ctx.HTML(200, "regit.html", gin.H{
-	// 	"file_name": header.Filename,
-	// 	"file_ary":  Csv_Result_2nd_ary,
-	// })
+	
 	ctx.HTML(200, "index.html", gin.H{
 		"file_name": header.Filename,
 		"file_ary":  Csv_Result_2nd_ary,
