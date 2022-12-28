987b8f6138198207cefb472a809fa20cfc643960
k-goto <k-goto@masagent.co.jp>
2022-12-28 17:32:15 +0900

    テキスト二次元配列格納前

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 
+
 $(function(){
    $('th').draggable({
       helper: 'clone',
@@ -14,11 +15,31 @@ $(function(){
       }
     });
     
-    $('#Preview_table th').draggable({
-      start: function(){
-        alert(9);
-     }
-	});  
+    $('#csv_syuturyoku').submit(function() {
+      var yoso_class_name = "";
+      var Show_ClassName_ary = [];
+      var T_class_name = "";
+      var judge = false
+      var yoso_id = "";
+      
+      $("#Preview_table").children().each(function(index, element){
+         $(this).children().each(function(index, element){
+            var tmp_ary =[];
+            $(this).children().each(function(index, element){
+               yoso_id = $(this).attr("id");
+               yoso_class_name = $(this).attr("class");
+               if ($('.' + yoso_class_name).css('display') != 'none') {
+                  tmp_ary.push($('#' + yoso_id).text());
+               } 
+             })
+             console.log(tmp_ary)
+          })
+        
+       })
+
+      return false;
+   
+  });
 });
 
 
@@ -29,17 +50,22 @@ function Clone_Yoso(copy_id) {
       $("#Edit_Table").clone().appendTo('#Preview').attr("id","Preview_table");
       $(".row_0 th:last").remove();
       $("#Preview_table tr th,#Preview_table tr td").hide();
+      
+      var i = 1;
       $("#Preview_table tr").children().each(function(index, element){
         var class_name = $(this).attr("class").replace("ui-draggable ui-draggable-handle","");
-        $(this).attr("class",class_name)
+        $(this).attr("class","ple_" + class_name);
+        $(this).attr("id","ple_" + class_name + "_r" + i);
+        i++;
       })
+      
 	}
 
    var el = document.getElementById('Preview_table');
    var dragger = tableDragger.default(el, {
-         dragHandler: "." + copy_id + "_02"
+         dragHandler: "." + "ple_" + copy_id + "_02"
    })
    
-   $("." + copy_id+"_02").show();
+   $("." + "ple_" + copy_id+"_02").show();
   
 }
